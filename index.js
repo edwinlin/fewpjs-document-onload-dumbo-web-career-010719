0d9562313e43b3b8faeb16ee8378b74a2b82a969
github username <github email address>
2019-02-19 20:08:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+//     let update = document.getElementById('text').textContent;
+//     console.log(update);
+//     update = "This is really cool!";
+// console.log(update);
+
+// document.addEventListener("DOMContentLoaded", domContentLoaded, false);
+
+// function domContentLoaded() {
+// 	// let update = document.getElementById('text').textContent;
+
+// 	// console.log(update)
+// 	updateDOM();
+// }
+
+// function updateDOM() {
+//     // do something again
+//     if (document.querySelector('p').textContent == "This is really cool!"){
+//     	document.querySelector('p').textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+//     }else if (document.querySelector('p').textContent == "JavaScript is so cool. It lets me add text to my page programmatically."){
+//     	document.querySelector('p').textContent = "This is really cool!";
+//     }
+// }
+
+function domContentLoaded() {
+	document.addEventListener("DOMContentLoaded", updateDOM);
+}
+function updateDOM() {
+    // do something again
+    if (document.querySelector('p').textContent == "This is really cool!"){
+    	document.querySelector('p').textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+    }else if (document.querySelector('p').textContent == "JavaScript is so cool. It lets me add text to my page programmatically."){
+    	document.querySelector('p').textContent = "This is really cool!";
+    }
+}
